34216d251d16fce8012f08004ac7b35e2e1fcf68
David Eriksson <davide@technocreatives.com>
2022-04-11 22:18:54 +0200

    Read from file or stdin

--- README.md
+++ README.md
@@ -2,11 +2,42 @@
 
 Generates random names from two or more lists of words. Written in Deno.
 
+## Example
+
+Given the input:
+
+```
+Fire
+Water
+Earth
+Wind
+
+Plant
+Cactus
+Tulip
+```
+
+
+The output is:
+
+```
+Fire Plant
+Fire Cactus
+Fire Tulip
+Water Plant
+Water Cactus
+Water Tulip
+Earth Plant
+Earth Cactus
+Earth Tulip
+Wind Plant
+Wind Cactus
+Wind Tulip
+```
+
 ## Usage
 
-``cp .env.example .env``
-
-deno run --allow-read main.ts
+deno run --allow-read main.ts example_words.txt
 
 You can optionally pass the --output argument to write the names to a file
 See --help for more info
--- main.ts
+++ main.ts
@@ -1,41 +1,39 @@
 import { Command } from "https://deno.land/x/cliffy/command/mod.ts";
-import { join } from "https://deno.land/std/path/mod.ts";
-import config from "./config.ts";
+import { readAll } from "https://deno.land/std/streams/conversion.ts";
 
-type Word = string
-type WordList = Word[]
+type Word = string;
+type WordList = Word[];
 
-const readSources = (fileName: string): WordList => {
-  const path = join(config.inputSourcesPath, fileName);
-  const inputSource = Deno.readTextFileSync(path);
-  const inputs = inputSource.split("\n");
-
-  return inputs;
-};
-
-const generateRandomNames = (sources: WordList[]): Word[] => {
-  if (sources.length === 0) {
-    throw new Error(`Received an empty word list. Does the path ${config.inputSourcesPath} contain any word list files?`)
+const generateRandomNames = (wordLists: WordList[]): Word[] => {
+  if (wordLists.length === 0) {
+    throw new Error(`Received an empty word list.`);
   }
 
-  if (sources.length === 1) {
-    return sources[0];
+  if (wordLists.length === 1) {
+    return wordLists[0];
   }
 
-  return sources[0].flatMap((input) =>
-    generateRandomNames(sources.slice(1, sources.length)).map((nextSource) => [input, nextSource].join(" "))
+  return wordLists[0].flatMap((word) =>
+    generateRandomNames(wordLists.slice(1, wordLists.length)).map((nextWord) => [word, nextWord].join(" "))
   );
 };
 
-async function main({ output }: { output?: string }) {
-  const files = Array.from(await Deno.readDirSync(config.inputSourcesPath))
-    .filter((file) => file.isFile)
-    .sort((a, b) => a.name.localeCompare(b.name));
+async function main({ filePath, output }: { filePath?: string; output?: string }) {
+  let words;
 
-  const inputs = files.map((file) => readSources(file.name));
-  const randomNames = generateRandomNames(inputs).join("\n");
+  if (filePath) {
+    const file = Deno.readTextFileSync(filePath);
+    words = file;
+  } else {
+    const stdinContent = await readAll(Deno.stdin);
+    const response = new TextDecoder().decode(stdinContent);
+    words = response;
+  }
 
-  console.log(randomNames)
+  const wordLists = words.split("\n\n").map((wordList) => wordList.split("\n"));
+  const randomNames = generateRandomNames(wordLists).join("\n");
+
+  console.log(randomNames);
 
   if (output) {
     Deno.writeTextFileSync(output, randomNames);
@@ -44,14 +42,16 @@ async function main({ output }: { output?: string }) {
 
 await new Command()
   .name("random-name-generator")
-  .description("A simple random name generator")
+  .description("A simple random name generator.")
+  .arguments("[file]")
   .version("v1.0.0")
   .option("-o, --output <filename>", "Where to save the randomly generated names", {
     default: "",
   })
-  .action(({ output }) => {
+  .action(({ output }, filePath) => {
     main({
       output,
+      filePath,
     });
   })
   .parse();
